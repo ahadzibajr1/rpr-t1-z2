e0c326a35ade928c09a95cd74f260cda35496b54
Hadzibajramovic Amila <ahadzibajr1@etf.unsa.ba>
2020-10-26 12:41:24 +0100

    Implementiran main

--- src/ba/unsa/etf/rpr/Main.java
+++ src/ba/unsa/etf/rpr/Main.java
@@ -16,6 +16,19 @@ public class Main {
 
     public static void main(String[] args) {
 
+        System.out.println("Unesite cijeli broj n: ");
+
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+
+        for (int i=0; i < n+1; i++) {
+            if (i % sumaCifara(i) == 0)
+                System.out.println("" + i + " ");
+
+        }
+
+
+
 
 
     }
